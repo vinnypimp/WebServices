b0210f0fce24be3142fb174718f685612864f8d8
vinnypimp <vpimpinella@gmail.com>
2016-03-29 00:10:52 -0400

    added data

--- AuthenticationService/Services/AuthSvc.svc.cs
+++ AuthenticationService/Services/AuthSvc.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 using AuthenticationService.Models;
+using AuthenticationService.Data;
 
 namespace AuthenticationService.Services
 {
@@ -12,13 +13,23 @@ namespace AuthenticationService.Services
     {
         public User ValidateUser()
         {
-            var dc = string.Empty;
+            AuthenticationDataContext dc = new AuthenticationDataContext();
             User results = new User();
 
-            foreach ()
-            results.UserName = 
+            foreach (appAuth_ValidateUserResult user in dc.appAuth_ValidateUser(AppName, UserName, Password))
+            {
+                results.UserName = user.UserName;
+                results.Email = user.Email;
+                results.Role = user.RoleName;
+                //results.FirstName
+                //results.LastName
+            };
 
-
+            return results;
         }
+
+        public string AppName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }

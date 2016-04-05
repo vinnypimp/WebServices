075ef175ac5b45ed9f2b851a059d39ab15ce94c4
vinnypimp <vpimpinella@gmail.com>
2016-04-05 00:00:54 -0400

    authsvcresults and data

---
+++ AuthenticationService/Models/AuthenticationResults.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace AuthenticationService.Models
+{
+    [DataContract]
+    [Serializable]
+        public class AuthenticationResults
+        {
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Role { get; set; }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public DateTime LastLoginDate { get; set; }
+
+        [DataMember]
+        public DateTime LastActivityDate { get; set; }
+
+        [DataMember]
+        public int ReturnCode { get; set; }
+
+        [DataMember]
+        public string ReturnReason { get; set; }
+
+        }
+}
